6136fc97ab46e36b444e78122622456f74cf4a32
fivewords <443672581@qq.com>
2018-07-10 15:20:02 +0800

    Initial commit

---
+++ simple/src/main/java/cn/dlj1/boot2/simple/config/Im.java
@@ -0,0 +1,7 @@
+package cn.dlj1.boot2.simple.config;
+
+public interface Im {
+
+    void write();
+
+}
---
+++ simple/src/main/java/cn/dlj1/boot2/simple/config/Impl.java
@@ -0,0 +1,9 @@
+package cn.dlj1.boot2.simple.config;
+
+public class Impl implements Im{
+
+    @Override
+    public void write() {
+        System.out.print("这是实现!");
+    }
+}
