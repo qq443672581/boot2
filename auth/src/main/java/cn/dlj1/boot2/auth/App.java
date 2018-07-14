7911e3b1654cf8a6c7332c6dcd94f30aec9aac8c
fivewords <443672581@qq.com>
2018-07-14 13:34:22 +0800

    init

---
+++ auth/src/main/java/cn/dlj1/boot2/auth/App.java
@@ -0,0 +1,13 @@
+package cn.dlj1.boot2.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+
+}
