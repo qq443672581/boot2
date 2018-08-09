6a485c43742ea9adb04ef28c3bc8a0457255394f
fivewords <443672581@qq.com>
2018-08-09 16:40:27 +0800

    Initial commit

---
+++ simple/src/main/resources/static/bar.js
@@ -0,0 +1,1 @@
+callback_("this is bar , <i-button @click='this.app.count++'>按钮</i-button>{{$router.app.count}}");
---
+++ simple/src/main/resources/static/foo.js
@@ -0,0 +1,1 @@
+callback_("this is foo  , <i-button @click='this.app.count++'>按钮</i-button><i-button @click='this.app.count++'>按钮</i-button><i-button @click='this.app.count++'>按钮</i-button><i-button @click='this.app.count++'>按钮</i-button>{{$router.app.count}}");
